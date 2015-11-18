c3acb6fd591fbd2190810f1c704f1182d04b2aec
zhjh1209 <zhjh1209@gmail.com>
2015-11-18 14:42:13 +0800

    A simple game build with Clojure Noir.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojurebreaker "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [noir "1.3.0-beta3"]
+                           [hiccup/hiccup "1.0.5"]
+                           [org.clojure/math.combinatorics "0.0.1"]]
+            :main clojurebreaker.server)
+
